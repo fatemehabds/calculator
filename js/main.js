407a9ba65fb5d37629a0b243732e0807db9e0917
fatemehabds <you@example.com>
2023-01-30 23:01:30 +0330

    end

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ let num1 = document.querySelector("input#num1");
 let num2 = document.querySelector("input#num2");
 let plus = document.querySelector("button#plus");
 plus.addEventListener("click", function () {
-  document.querySelector("h1").innerHTML =
+  document.querySelector("h2").innerHTML =
     Number(num1.value) + Number(num2.value);
+});
+let div = document.querySelector("button#div");
+div.addEventListener("click", function () {
+  document.querySelector("h2").innerHTML =
+    Number(num1.value) / Number(num2.value);
+});
+let minus = document.querySelector("button#minus");
+minus.addEventListener("click", function () {
+  document.querySelector("h2").innerHTML =
+    Number(num1.value) - Number(num2.value);
+});
+let mul = document.querySelector("button#mul");
+mul.addEventListener("click", function () {
+  document.querySelector("h2").innerHTML =
+    Number(num1.value) * Number(num2.value);
 });
